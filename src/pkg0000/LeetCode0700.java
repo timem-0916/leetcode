ba2d35fc831421a15f2070f6a17780fe3ad673bb
timem <timem0916@icloud.com>
2026-09-01 18:26:37 +0800

    700. 二叉搜索树中的搜索

---
+++ src/pkg0000/LeetCode0700.java
@@ -0,0 +1,25 @@
+package pkg0000;
+
+import util.TreeNode;
+
+public class LeetCode0700 {
+    /**
+     * 700. 二叉搜索树中的搜索
+     * @param root
+     * @param val
+     * @return
+     */
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        
+        if (val == root.val) {
+            return root;
+        } else if (val < root.val) {
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
