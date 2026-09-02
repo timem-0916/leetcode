3a904addf39f4597bbb3e65c8daab7cd0a71bcd5
timem <timem0916@icloud.com>
2026-09-02 10:57:26 +0800

    450. 删除二叉搜索树中的节点

---
+++ src/pkg0000/LeetCode0400.java
@@ -0,0 +1,40 @@
+package pkg0000;
+
+import util.TreeNode;
+
+public class LeetCode0400 {
+    /**
+     * 450. 删除二叉搜索树中的节点
+     * @param root
+     * @param key
+     * @return
+     */
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            TreeNode successor = root.right;
+            while (successor.left != null) {
+                successor = successor.left;
+            }
+            root.right = deleteNode(root.right, successor.val);
+            successor.right = root.right;
+            successor.left = root.left;
+            return successor;
+        }
+
+        return root;
+    }
+}
