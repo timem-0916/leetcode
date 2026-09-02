2bf02a1275c91f8946d0a9daf6fc2e2551eb1ce0
timem <timem0916@icloud.com>
2026-09-02 10:57:12 +0800

    3875. 构造奇偶一致的数组 I

---
+++ src/pkg3000/LeetCode3800.java
@@ -0,0 +1,12 @@
+package pkg3000;
+
+public class LeetCode3800 {
+    /**
+     * 3875. 构造奇偶一致的数组 I
+     * @param nums1
+     * @return
+     */
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
