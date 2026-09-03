82214a949466af8bcfbfbfc3a7a2a5945d2a78a1
timem <timem0916@icloud.com>
2026-09-03 11:04:15 +0800

    3876. 构造奇偶一致的数组 II

--- src/pkg3000/LeetCode3800.java
+++ src/pkg3000/LeetCode3800.java
@@ -9,4 +9,23 @@ public class LeetCode3800 {
     public boolean uniformArray(int[] nums1) {
         return true;
     }
+
+    /**
+     * 3876. 构造奇偶一致的数组 II
+     * @param nums1
+     * @return
+     */
+    public boolean uniformArrayII(int[] nums1) {
+        int n = nums1.length, minV = nums1[0];
+        int[] cnt = new int[2];
+        for (int num : nums1) {
+            minV = Math.min(minV, num);
+            if ((num & 1) == 1) {
+                cnt[0]++;
+            } else {
+                cnt[1]++;
+            }
+        }
+        return (minV & 1) == 1 || cnt[1] == n;
+    }
 }
