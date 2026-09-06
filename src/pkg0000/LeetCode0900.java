13945c960fb572d65e6811b42efe977476573699
timem <timem0916@icloud.com>
2026-09-07 04:17:35 +0800

    940. 不同的子序列 II，继续优化的动态规划

--- src/pkg0000/LeetCode0900.java
+++ src/pkg0000/LeetCode0900.java
@@ -11,19 +11,13 @@ public class LeetCode0900 {
     public int distinctSubseqII(String s) {
         final int MOD = 1000000007;
         int[] g = new int[26];
-        int n = s.length();
+        int n = s.length(), total = 0;
         for (int i = 0; i < n; i++) {
-            int total = 1;
-            for (int j = 0; j < 26; j++) {
-                total = (total + g[j]) % MOD;
-            }
-            g[s.charAt(i) - 'a'] = total;
+            int oi = s.charAt(i) - 'a';
+            int prev = g[oi];
+            g[oi] = (total + 1) % MOD;
+            total = ((total + g[oi] - prev) % MOD + MOD) % MOD;
         }
-
-        int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            ans = (ans + g[i]) % MOD;
-        }
-        return ans;
+        return total;
     }
 }
