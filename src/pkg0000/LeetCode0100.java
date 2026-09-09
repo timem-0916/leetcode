67919c2c47df4b893a1877eae5cda5c2f115a318
timem <timem0916@icloud.com>
2026-09-09 21:16:50 +0800

    139. 单词拆分

--- src/pkg0000/LeetCode0100.java
+++ src/pkg0000/LeetCode0100.java
@@ -1,6 +1,9 @@
 package pkg0000;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class LeetCode0100 {
     /**
@@ -59,6 +62,28 @@ public class LeetCode0100 {
         }
 
         return Arrays.stream(candies).sum();
+    }
+
+    /**
+     * 139. 单词拆分
+     * @param s
+     * @param wordDict
+     * @return
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>(wordDict);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 
     /**
