5948df1f83112a660c5ddb0eb97d1316c787ba55
timem <timem0916@icloud.com>
2026-09-10 19:39:25 +0800

    877. 石子游戏

--- src/pkg0000/LeetCode0800.java
+++ src/pkg0000/LeetCode0800.java
@@ -67,4 +67,26 @@ public class LeetCode0800 {
         }
         return true;
     }
+
+    /**
+     * 877. 石子游戏
+     * @param piles
+     * @return
+     */
+    public boolean stoneGame(int[] piles) {
+        int n = piles.length;
+        // dp[i][j] 表示当剩下的石子堆为下标 i 到下标 j 时，即在下标范围 [i,j] 中，当前玩家与另一个玩家的石子数量之差的最大值，注意当前玩家不一定是先手 Alice
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+
+        for (int l = 2; l <= n; l++) {
+            for (int i = 0; i <= n - l; i++) {
+                int j = i + l - 1;
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
 }
