283f1a0e56ec12106b38f82462145c21926859ea
timem <timem0916@icloud.com>
2026-09-10 16:08:51 +0800

    292. Nim 游戏

---
+++ src/pkg0000/LeetCode0200.java
@@ -0,0 +1,12 @@
+package pkg0000;
+
+public class LeetCode0200 {
+    /**
+     * 292. Nim 游戏
+     * @param n
+     * @return
+     */
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
